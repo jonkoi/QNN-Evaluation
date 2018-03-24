4582e3a7d66def432bc585ff8fcd7137050dbeff
Vincent-Xiao <vincent.xiao.me@gmail.com>
2018-03-24 17:44:45 +0800

    update

--- nnUtils.py
+++ nnUtils.py
@@ -213,7 +213,7 @@ def Residual(moduleList, name='Residual'):
 
 def Block(nOutputPlane, kW, kH, dW=1, dH=1,
         padding='VALID', bias=True, name='Block',reuse=None,isResudual=True):
-    def block_layer():
+    def block_layer(x,is_training=True):
         with tf.variable_scope(name, reuse=reuse):
             curr_layers = [
                 BatchNormalization(),
@@ -224,19 +224,19 @@ def Block(nOutputPlane, kW, kH, dW=1, dH=1,
                 SpatialConvolution(nOutputPlane,kW,kH,dW,dH, padding=padding,bias=bias)
             ]
             if isResudual:
-                return Residual(curr_layers)
+                return [Residual(curr_layers)]
             else:
                 return curr_layers
     return block_layer
 
 def Group(nOutputPlane, kW, kH, dW=1, dH=1,K=10,N=4,
         padding='VALID', bias=True, name='Group',reuse=None):# K:Network Width;N:GroupNum
-    def group_layer():
+    def group_layer(x,is_training=True):
         with tf.variable_scope(name, reuse=reuse):
             modules = []
             for i in xrange(0,N):
                 if i==0:
-                    modules+=Block(nOutputPlane*K,kW,kH,dW,dH,padding=padding,bias=bias,reuse=reuse,isResudual=False)
+                    modules +=[Block(nOutputPlane*K,kW,kH,dW,dH,padding=padding,bias=bias,reuse=reuse,isResudual=False)]
                 else:
                     modules += [Block(nOutputPlane*K,kW,kH,dW,dH,padding=padding,bias=bias,reuse=reuse)]
         return Sequential(modules)

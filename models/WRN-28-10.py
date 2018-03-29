baf152f0b2a15a6637338815f6c85e6ae8a4fa3e
Vincent-Xiao <vincent.xiao.me@gmail.com>
2018-03-29 21:55:35 +0800

    update

--- models/WRN-28-10.py
+++ models/WRN-28-10.py
@@ -8,9 +8,9 @@ model = Sequential([
     SpatialConvolution(16,3,3,padding='SAME'),
     BatchNormalization(),
     ReLU(),
-    Block(16,3,3,K=K,N=N,padding='SAME',fixPaddingFilters=16*K-16),
-    Block(32,3,3,2,2,K=K,N=N,padding='SAME',fixPaddingFilters=16*K), #32-16
-    Block(64,3,3,2,2,K=K,N=N,padding='SAME',fixPaddingFilters=32*K), #64-32
+    Block(16,3,3,K=K,N=N,padding='SAME'),
+    Block(32,3,3,2,2,K=K,N=N,padding='SAME'),
+    Block(64,3,3,2,2,K=K,N=N,padding='SAME'),
     SpatialAveragePooling(8,8,1,1),
     Affine(10)
 ])
--- nnUtils.py
+++ nnUtils.py
@@ -202,24 +202,34 @@ def Concat(moduleList, dim=3):
         return output
     return model
 
-def Residual(moduleList, name='Residual',fixPaddingFilters=0,fixPaddingStride=1):
+def Residual(moduleList, name='Residual',fixShape=['pad',1,1]):
+    #fixShape:fixShape if input filters !=output filters
+    #params=['pad' or 'conv',stride,stride]
+    #'pad' or 'conv':fixshape method:conv1x1 or pooling1x1+padiing;
+    #stride:stride for pooling
     m = Sequential(moduleList)
     def model(x, is_training=True):
     # Create model
         with tf.variable_op_scope([x], None, name):
-            orig_x=x
-            with tf.variable_op_scope(None, 'fixPadding', reuse=None):
-                if fixPaddingFilters > 0:#zero padding input if input channels ~= output channels
-                    x=tf.nn.avg_pool(x, ksize=[1, 1, 1, 1], strides=[1, fixPaddingStride,fixPaddingStride, 1],padding='VALID')
-                    x=tf.pad(x,[[0, 0], [0, 0], [0, 0],[fixPaddingFilters//2, fixPaddingFilters//2]])
-            output = tf.add(m(orig_x, is_training=is_training), x)
+            output=m(x,is_training=is_training)
+            with tf.variable_op_scope(None, 'fixShape', reuse=None):
+                filterIn=x.get_shape()[3]
+                filterOut=output.get_shape()[3]
+                if filterIn !=filterOut:
+                    if fixShape[0]=='pad':
+                        x=tf.nn.avg_pool(x, ksize=[1, 1, 1, 1], strides=[1, fixShape[1],fixShape[2],1],padding='VALID')
+                        x=tf.pad(x,[[0, 0], [0, 0], [0, 0],[(filterOut-filterIn)//2, (filterOut-filterIn)//2]])
+                    else:#conv method
+                        w = tf.get_variable('weight', [1, 1, filterIn, filterOut],
+                            initializer=tf.contrib.layers.xavier_initializer_conv2d())
+                        x = tf.nn.conv2d(x, w, strides=[1, fixShape[1],fixShape[2], 1], padding='SAME')
+            output = tf.add(output, x)
             return output
     return model
 
 def Residual_func(nOutputPlane, kW, kH, dW=1, dH=1,
-        padding='VALID', bias=True, name='Residual_func',reuse=None,fixPaddingFilters=0,type='basic',bottleWidth=2):
-    def model(x, is_training=True):
-        with tf.variable_op_scope([x],None, name, reuse=reuse):
+        padding='VALID', bias=True, name='Residual_func',reuse=None,fixShapeMethod='pad',type='basic',bottleWidth=2):
+        with tf.variable_op_scope(None,None, name, reuse=reuse):
             if type=='basic':
                 curr_layers = [
                     SpatialConvolution(nOutputPlane,kW,kH,dW,dH, padding=padding,bias=bias),
@@ -269,26 +279,23 @@ def Residual_func(nOutputPlane, kW, kH, dW=1, dH=1,
                 ]
             modules = []
             if 'pre' in type:
-                modules=[Residual(curr_layers,fixPaddingFilters=fixPaddingFilters,fixPaddingStride=dW)]
+                modules=[Residual(curr_layers,fixShape=[fixShapeMethod,dW,dH])]
             else:
-                modules=[Residual(curr_layers,fixPaddingFilters=fixPaddingFilters,fixPaddingStride=dW)]+[ReLU()]
-            m=Sequential(modules)
-            output=m(x,is_training=is_training)
-            return output
-    return model
+                modules=[Residual(curr_layers,fixShape=[fixShapeMethod,dW,dH])]+[ReLU()]
+        return modules
 
 def Block(nOutputPlane, kW, kH, dW=1, dH=1,K=10,N=4,padding='VALID', bias=True, name='Block',reuse=None,
-        fixPaddingFilters=0,bottleWidth=2):# K:Network Width;N:GroupNum
+        fixShapeMethod='pad',bottleWidth=2):# K:Network Width;N:GroupNum
     def model(x, is_training=True):
         with tf.variable_op_scope([x],None,name,reuse=reuse):
             modules = []
             for i in xrange(0,N):
                 if i==0:
-                    modules +=[Residual_func(nOutputPlane*K,kW,kH,dW,dH,padding=padding,bias=bias,
-                        reuse=reuse,fixPaddingFilters=fixPaddingFilters,bottleWidth=bottleWidth)]
+                    modules +=Residual_func(nOutputPlane*K,kW,kH,dW,dH,padding=padding,bias=bias,
+                        reuse=reuse,fixShapeMethod=fixShapeMethod,bottleWidth=bottleWidth)
                 else:
-                    modules += [Residual_func(nOutputPlane*K,kW,kH,1,1,padding=padding,bias=bias,
-                        reuse=reuse,fixPaddingFilters=0,bottleWidth=bottleWidth)]
+                    modules += Residual_func(nOutputPlane*K,kW,kH,1,1,padding=padding,bias=bias,
+                        reuse=reuse,fixShapeMethod=fixShapeMethod,bottleWidth=bottleWidth)
             m=Sequential(modules)
             output=m(x,is_training=is_training)
             return output
